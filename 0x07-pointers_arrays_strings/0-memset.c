f72702c2ff1a400260b2fc70a164744519b95cb6
KojoCyrus <kaasante796@gmail.com>
2023-04-03 15:49:27 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,22 @@
-include "main.h"
+#include "main.h"
 
 /**
 *_memset - fills the first n bytes of the memory area
 *@s is a memory area
-*@b is a value
-*@n is the number of bytes
+*@b is a value that is going to be copied
+*@n number of times to for b to be copied
 *
-*Return: This should return the changed array with new value for n
+*Return: s
 */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	for (; n > 0; i++)
+	for (i = 0; i > n; i++)
 
 	{
 	s[i] = b;
-	n--;
 	}
 
 	return (s);
