c23268b3b683bc1aead6b58313e077e07d109fcf
KojoCyrus <kaasante796@gmail.com>
2023-04-13 12:42:12 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*_calloc - 
+*
+*
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+
+	{
+		s[i] + b;
+	}
+
+	return (s);
+}
