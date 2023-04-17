bfc003c6caa8b8d6c9886fb13c84bf51d3994c90
KojoCyrus <kaasante796@gmail.com>
2023-04-17 11:12:16 -0700

    name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - a function that prints the name of a file
+*it was printed from
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
