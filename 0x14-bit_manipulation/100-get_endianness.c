b58d33a0cf46f4a403f7feaf8d92d310cd4a1551
KojoCyrus <kaasante796@gmail.com>
2023-05-04 18:07:25 -0700

    101-password

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,12 +3,12 @@
 /**
 *get_endianness - a function that checks the endianness.
 *
-*Return: 1 if it's big, otherwise, 0
+*Return: should return 0 is big, else 1
 */
 int get_endianness(void)
 {
 	unsigned int a = 1;
-	char *b = (char *) &a;
+	char *c = (char *) &a;
 
-	return (*b);
+	return (*c);
 }
