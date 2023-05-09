0e7d51d4dc36f423ddba6a4526eb60e0572d682f
KojoCyrus <kaasante796@gmail.com>
2023-05-08 18:46:54 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
@@ -30,7 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (a == -1 || w == -1)
 		return (-1);
-	close (a);
+	close(a);
 
 	return (1);
 }
