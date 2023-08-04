16fcd6dc37cea7f23294e9cfe143ed63b2232ab0
KojoCyrus <kaasante796@gmail.com>
2023-08-04 13:57:18 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,6 +4,8 @@
  *get_bit - a function that returns the value of a bit at a given index
  *@n: number to be searched
  *@index: index of the bit
+ *
+ *Return: the value of the bit
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
